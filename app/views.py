aee01ae7da59444fc466835a4055b5af63c78b96
vikranth0212 <vikranth15.6.1995@gmail.com>
2023-11-22 18:23:15 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def vikranth(request):
+    return render(request,'vikranth.html')
+
+def tajmahal(request):
+    return render(request,'tajmahal.html')    
